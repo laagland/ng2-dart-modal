f77d48e07c6e4e043330bce89496e9de80886202
Louis Laagland <louis@wunderful.nl>
2016-02-03 12:06:36 +0000

    icustommodal.dart edited online with Bitbucket

--- lib/shared/models/icustommodal.dart
+++ lib/shared/models/icustommodal.dart
@@ -1,1 +1,24 @@
-part of schemas.dart
+part of schemas
+
+/**
+ * A Type used as a binding key for dialog window Components
+ */
+class ICustomModal {
+	
+	ICustomModal();
+}
+
+//TODO: implement as interface
+class ICustomModalComponent {
+    ModalDialogInstance dialog;
+
+    /**
+     * Invoked before a modal is dismissed, return true to cancel dismissal.
+     */
+    bool beforeDismiss();
+
+    /**
+     * Invoked before a modal is closed, return true to cancel closing.
+     */
+    bool beforeClose();
+}
