2d1f1c96ba7b4277ba2df90f6d2c3e070eec6277
Louis Laagland <louis@wunderful.nl>
2016-02-04 10:33:51 +0000

    modelbackdrop.dart edited online with Bitbucket

--- lib/app/components/modelbackdrop.dart
+++ lib/app/components/modelbackdrop.dart
@@ -1,1 +1,44 @@
 part of ng2_dart_modal;
+
+/**
+ * Represents the modal backdrop.
+ */
+@Component(
+    selector: 'modal-backdrop',
+    host: {
+        '[style.position]': 'position',
+        '[style.height]': 'height',
+        '[style.width]': 'width',
+        '[style.top]': 'top',
+        '[style.left]': 'left',
+        '[style.right]': 'right',
+        '[style.bottom]': 'bottom'
+
+    },
+    template: '''
+		<div [style.position]="position" class="in modal-backdrop" #modalBackdrop></div>
+		'''
+)
+
+class ModalBackdrop {
+    public position: string;
+    public height: string;
+    public width: string;
+    public top: string;
+    public left: string;
+    public right: string;
+    public bottom: string;
+
+
+    ModalBackdrop(ModalDialogInstance dialog) {
+        if (!dialog.inElement) {
+            this.position = this.width = this.height = null;
+            this.top = this.left = this.right = this.bottom = null;
+        } else {
+            this.position = 'absolute';
+            this.height = '100%';
+            this.width = '100%';
+            this.top = this.left = this.right = this.bottom = '0';
+        }
+    }
+}
